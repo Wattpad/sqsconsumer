5833e01550698a924f5a89db793323dc61b9438b
Jonathan Harlap <jharlap@gmail.com>
2016-09-06 17:04:13 -0400

    Remove go-kit dependency
    It was only used in the TrackMetrics middleware which we don't use
    anywhere and which was anemic enough that users should really always
    implement their own infrastructure-appropriate version anyway.

--- middleware/metrics.go
+++ middleware/metrics.go
@@ -1,29 +1,24 @@
 package middleware
 
 import (
+	"expvar"
 	"time"
 
 	"github.com/Wattpad/sqsconsumer"
-	"github.com/go-kit/kit/metrics"
-	"github.com/go-kit/kit/metrics/expvar"
+	"github.com/Wattpad/sqsconsumer/middleware/movingaverage"
 	"golang.org/x/net/context"
 )
 
 // TrackMetrics decorates a MessageHandler to collect metrics about successes, failures and runtime reports in ms*10.
-func TrackMetrics(successVarName, failureVarName, timingVarName string) MessageHandlerDecorator {
-	successes := expvar.NewCounter(successVarName)
-	failures := expvar.NewCounter(failureVarName)
-
-	// histogram from 0-100000ms with 3 sigfigs tracking 50, 95 and 99 %iles
-	h := expvar.NewHistogram(timingVarName, 0, 100000, 3, 50, 95, 99)
-
-	timing := metrics.NewTimeHistogram(time.Millisecond, h)
+func TrackMetrics(successes, failures *expvar.Int, timing *expvar.Float) MessageHandlerDecorator {
+	ema := movingaverage.New(5 * time.Second)
 
 	return func(fn sqsconsumer.MessageHandlerFunc) sqsconsumer.MessageHandlerFunc {
 		return func(ctx context.Context, msg string) error {
 			start := time.Now()
 			defer func() {
-				timing.Observe(time.Since(start))
+				v := ema.Update(time.Since(start).Seconds() * 1000)
+				timing.Set(v)
 			}()
 
 			err := fn(ctx, msg)
